69db59eca9156e971211722bafe7320333713a8c
John Ziniti <rejpz@channing.harvard.edu>
2021-07-18 14:39:30 -0400

    Generating NWDID lists for all studies

---
+++ scripts/python/read_sapphire_aliases.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+#### BOILERPLATE
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.orm import sessionmaker
+sapphire8_md = MetaData(schema='SAPPHIRE8')
+engine = create_engine('oracle://sapphire_r:sapphire_r0@172.27.104.202:1521/labvprd')
+Session = sessionmaker(bind=engine)
+session = Session()
+SDIAlias = Table('SDIALIAS', sapphire8_md, autoload=True, autoload_with=engine)
+##### BOILERPLATE
+
+alias_prefix = snakemake.params.get('alias_prefix', 'NWD')
+s_studyid = snakemake.params.get('s_studyid', None)
+if s_studyid:
+    nwdids = pd.read_sql(session.query(SDIAlias).where(SDIAlias.c.aliasid.like(f'{alias_prefix}%')).filter(SDIAlias.c.keyid2==s_studyid).statement, engine)
+else:
+    nwdids = pd.read_sql(session.query(SDIAlias).where(SDIAlias.c.aliasid.like(f'{alias_prefix}%')).statement, engine)
+## nwdids = nwdids.rename(columns={'keyid1':'S_SAMPLEID'})
+nwdids.to_csv(snakemake.output.nwdids, index=False, columns=['aliasid',])
---
+++ workflows/freeze.10.smk
@@ -0,0 +1,23 @@
+STUDIES = ['CAMP',
+           'CRA',
+           'ECLPSE',
+           'EOCOPD',
+           'GECOPD',
+           'GLAXO',
+           'LTCOPD',
+           'LTRC',
+           'PLCOPD'
+]
+TARGETS = expand("tmp/{s_studyid}.nwds.txt", s_studyid=STUDIES)
+
+rule: input: TARGETS
+
+rule sapphire_nwd_list:
+    input: 
+    output: nwdids=temp("tmp/{s_studyid}.nwds.txt")
+    conda: "../envs/sapphire8.yaml"
+    params:
+        alias_prefix='NWD',
+        s_studyid='{s_studyid}',
+    script: "../scripts/python/read_sapphire_aliases.py"
+
