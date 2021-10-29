05742c8d0be95fb47730f92c62482c7958396798
Alexander  Kaufmann <reaka@nantucket.bwh.harvard.edu>
2021-10-29 14:53:46 -0400

    integrated GenerateGraph class with snakemake workflow

--- scripts/python/image_plotter.py
+++ scripts/python/image_plotter.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import networkx as nx
+from itertools import combinations
+import math
 
 class GenerateGraph:
     # initialize object with manifest data and cdata
@@ -7,7 +10,7 @@ class GenerateGraph:
         self.df = manifest_df
         self.dfc = cdata_df
         self.node_mappings = { 
-            "ObservedGender": { "F": "o", "M": "s", "U": "d", "": "v" },
+            "ObservedGender": { "F": "o", "M": "s", "U": "d", "nan": "v" },
             "SampleLabel": { "Number": "s", "NWD": "d", "TOE": "v" },
             "SampleAlias": { "Number": "s", "NWD": "d", "TOE": "v" },
             "SampleTypeCode": { "d": "s", "m": "p", "r": "d" },
@@ -23,11 +26,11 @@ class GenerateGraph:
             "S_SAMPLEID": lambda x: x[0:3] + x[4]
         }
     
+    # method to call to generate graph
     def graph(self, group_id, shape_by):
         # filtering by group_id
         query_groupid = f'group_id == {group_id}'
         df_filter_groupid = self.df.query(query_groupid)
-
         # get nodes
         nodes = df_filter_groupid["SampleLabel"].values
 
@@ -36,13 +39,11 @@ class GenerateGraph:
 
         # getting all combinations of samplelabels
         edges, labels = self.generate_edges(nodes)
-        print(nodes)
 
         self.generate_graph(nodes, shape_nodes, edges, labels, group_id)
         
     def generate_graph(self, nodes, shape_nodes, edges, labels, groupid):
-        print(shape_nodes)
-        graph_path = "GraphOutputs/" + "GroupId" + str(groupid) + "Graph.png"
+        graph_path = "images/" + "group" + str(groupid) + ".png"
 
         fig = plt.figure(figsize=(12,12))
         ax = plt.subplot(111)
@@ -54,12 +55,14 @@ class GenerateGraph:
         pos = nx.spring_layout(G, k=0.6)
 
         for shape in shape_nodes.keys():
-            nx.draw_networkx_nodes(G, pos, nodelist=shape_nodes[shape], node_shape=shape, label=shape)
+            nx.draw_networkx_nodes(G, pos, nodelist=shape_nodes[shape]["nodes"], node_shape=shape, label=shape_nodes[shape]["attribute"])
             nx.draw_networkx_labels(G, pos, labels={node:node.split("_")[0] for node in G.nodes()})
     
         nx.draw_networkx_edges(G, pos, edgelist=edges)
         nx.draw_networkx_edge_labels(G,pos,edge_labels=labels,font_color='red',label_pos=0.4)
         plt.legend()
+        plt.savefig(graph_path, format="PNG")
+        plt.close(fig)
         
     # returns a list of edges and a list of edge labels
     # given a list of nodes
@@ -79,7 +82,7 @@ class GenerateGraph:
                 
         return filtered_edges, labels
     
-    # returns a dictionary with nodes grouped by shape such as { "shape": [nodes]... }
+    # returns a dictionary with nodes grouped by shape such as { "shape": { "nodes": [nodes], "attribute": "attr" }... }
     # given a list of nodes
     def map_nodes(self, n, by):
         shape_nodes = {}
@@ -92,19 +95,20 @@ class GenerateGraph:
         for node in n:
             # get attribute for node
             query_string = f'SampleLabel == "{node}"'
-            attr = self.df.query(query_string)[by].values[0]
-            
+            # cast to String for nan case
+            attr = str(self.df.query(query_string)[by].values[0])
+
             # get final attribute String
             # will not throw exception because of try except above
             final_attr = self.mapping_funcs[by](attr)
-            
+
             # get mapped shape
             shape = curr_map[final_attr]
             
             if shape in shape_nodes:
-                shape_nodes[shape].append(node)
+                shape_nodes[shape]["nodes"].append(node)
             else:
-                shape_nodes[shape] = [node]
+                shape_nodes[shape] = { "nodes": [node], "attribute": final_attr }
                 
         return shape_nodes
     
@@ -129,8 +133,8 @@ class GenerateGraph:
             return "Number"
         
     def dataset_id_func(self, attr):
-        num = attr
-        
+        num = int(attr)
+
         if num == 0:
             return "tmp0"
         elif num == 1:
@@ -140,12 +144,38 @@ class GenerateGraph:
         elif num == 3:
             return "tmp3"
 
+# concord values must be floats, or nan(some c_data files have strings in concord column)
+# assuming that c_data has concord value for both combinations of ID1 and ID2 (line 74)
+# S_SAMPLEID must be in format "tmp/#..."
 
 if __name__ == "__main__":
-    print("hello world")
+    if 'snakemake' in locals():
+        group_ids = snakemake.params.group_ids
+        shape_by = snakemake.params.shape_by
+        manifest_path = snakemake.input.annotated_manifest
+        c_data_path = snakemake.input.concordance_data
+    else:
+        manifest_path = "/proj/regeps/regep00/studies/TopMed/data/dna/whole_genome/TopMed/data/freezes/freeze.10.cdnm/plate103/ANNOTATED_MANIFEST.csv"
+        c_data_path = "/proj/regeps/regep00/studies/TopMed/data/dna/whole_genome/TopMed/data/freezes/freeze.10.cdnm/plate103/c_data.build_sample_groups"
+        shape_by = "dataset_id"
+        group_ids = [1009, 1010, 102, 1081, 1088]
     
-    group_id = snakemake.params.group_ids
+    df = pd.read_csv(manifest_path)
+    dfc = pd.read_csv(c_data_path)
     
-    file_path = f'images/{group_id}.txt'
-    f = open(file_path, "a")
-    f.close()
+    myGraph = GenerateGraph(df, dfc)
+    
+    if group_ids[0] == "all":
+        # satisfy requirement for outputing all.txt file
+        f = open("images/groupall.png", "a")
+        f.write(str(len(group_ids)))
+        f.close()
+        
+        # generate graphs for all group ids
+        all_groups = df["group_id"].unique()
+        for group in all_groups:
+            myGraph.graph(group, shape_by)
+        
+    else:
+        for group_id in group_ids:
+            myGraph.graph(group_id, shape_by)
